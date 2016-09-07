4acbfa1b1d05f1e982404b021edfb34e9d1a5dd1
Jonathan Norstrom <jon.norstrom@gmail.com>
2016-09-07 10:28:01 -0500

    fixed all my refactor-fuckery - not gonna happen

--- public/application.js
+++ public/application.js
@@ -1,108 +1,105 @@
-var animatorObject = {
-  captionLength: 0,
-  captionOptions: ["Oh, hello there.", "So this is my page.", "Hope you get what you need :)"],
-  captionIndex: 0,
-  rgbOptions: ["rgb(61, 183, 88)", "rgb(230, 79, 59)", "rgb(222, 77, 99)", "rgb(211, 188, 180)", "rgb(255, 255, 255)", "rgb(255, 220, 90)", "rgb(77, 222, 171)"],
-  colorIndex: 0,
-  currentlyRaving: false,
+var captionLength = 0;
+var captionOptions = ["Oh, hello there.", "So this is my page.", "Hope you get what you need :)"]
+var captionIndex = 0;
+var rgbOptions = ["rgb(61, 183, 88)", "rgb(230, 79, 59)", "rgb(222, 77, 99)", "rgb(211, 188, 180)", "rgb(255, 255, 255)", "rgb(255, 220, 90)", "rgb(77, 222, 171)"]
+var colorIndex = 0;
 
-  cursorAnimation() {
-    $('#cursor').animate({opacity: 0}, 400).animate({opacity: 1}, 400);
-    setTimeout(this.cursorAnimation, 800)
-    return "hello"
-  },
+var currentlyRaving = false;
 
-  runCaptions() {
-    debugger;
-    this.cursorAnimation();
-    $caption = $('#caption');
-    setTimeout(this.showCaptions, 1000);
-  },
-
-  type() {
-      $caption.html(captionToType.substr(0, captionLength++));
-      if(captionLength < captionToType.length+1) {
-          setTimeout('type()', 70);
-      }
-  },
-
-  erase() {
-      $caption.html(captionToType.substr(0, captionLength--));
-      if(captionLength >= 0) {
-          setTimeout('erase()', 50);
-      }
-  },
-
-  => () 
-
-  showCaptions() {
-    captionToType = this.captionOptions[captionIndex];
-    type();
-    if (captionIndex < (captionOptions.length - 1)) {
-      captionIndex++
-      setTimeout('erase()', 4000);
-      setTimeout('showCaptions()', 6000)
-    } else {
-      setTimeout(function(){
-        $('#cursor').remove()
-      }, 1500)
-    }
-  },
-
-  toggleMethods() {
-    $('.methods').toggle('slide');
-  },
-
-  switchRaveState() {
-    if (currentlyRaving) {
-      currentlyRaving = false
-    } else {
-      currentlyRaving = true
-    }
-  },
-
-  checkRaveState() {
-    if (currentlyRaving) {
-      animateBackgroundColor(60);
-      play();
-      setTimeout('checkRaveState()', 100);
-    } else {
-      stopAndReset();
-    }
-  },
-
-  animateBackgroundColor(speed) {
-    if (colorIndex == rgbOptions.length) {
-      colorIndex = 0;
-    }
-    $('html').animate({"background-color": rgbOptions[colorIndex]}, speed);
-    colorIndex++
-  },
-
-  play() {
-    document.getElementById("audio").play();
-  },
-
-  stopAndReset() {
-    var audio = document.getElementById("audio");
-    audio.pause();
-    audio.currentTime = 0
-  },
+function runCaptions() {
+  cursorAnimation();
+  $caption = $('#caption');
+  setTimeout('showCaptions()', 1000);
 }
 
+function cursorAnimation() {
+  $('#cursor').animate({
+      opacity: 0
+  }, 400).animate({
+      opacity: 1
+  }, 400);
+  setTimeout('cursorAnimation()')
+}
 
+function type() {
+    $caption.html(captionToType.substr(0, captionLength++));
+    if(captionLength < captionToType.length+1) {
+        setTimeout('type()', 70);
+    }
+}
+
+function erase() {
+    $caption.html(captionToType.substr(0, captionLength--));
+    if(captionLength >= 0) {
+        setTimeout('erase()', 50);
+    }
+}
+
+function showCaptions() {
+  captionToType = captionOptions[captionIndex];
+  type();
+  if (captionIndex < (captionOptions.length - 1)) {
+    captionIndex++
+    setTimeout('erase()', 4000);
+    setTimeout('showCaptions()', 6000)
+  } else {
+    setTimeout(function(){
+      $('#cursor').remove()
+    }, 1500)
+  }
+}
+
+function toggleMethods() {
+  $('.methods').toggle('slide');
+}
+
+function switchRaveState() {
+  if (currentlyRaving) {
+    currentlyRaving = false
+  } else {
+    currentlyRaving = true
+  }
+}
+
+function checkRaveState() {
+  if (currentlyRaving) {
+    animateBackgroundColor(60);
+    play();
+    setTimeout('checkRaveState()', 100);
+  } else {
+    stopAndReset();
+  }
+}
+
+function animateBackgroundColor(speed) {
+  if (colorIndex == rgbOptions.length) {
+    colorIndex = 0;
+  }
+  $('html').animate({"background-color": rgbOptions[colorIndex]}, speed);
+  colorIndex++
+}
+
+function play() {
+  document.getElementById("audio").play();
+}
+
+function stopAndReset() {
+  var audio = document.getElementById("audio");
+  audio.pause();
+  audio.currentTime = 0
+}
 
 $(function(){
-  animatorObject.runCaptions();
+  runCaptions();
 
   $('.message a').on('click', function() {
     event.preventDefault()
-    animatorObject.toggleMethods();
+    toggleMethods();
   })
 
   $('.color-change p.background').on('click', function() {
     event.preventDefault();
-    animatorObject.animateBackgroundColor(350)
+    animateBackgroundColor(350)
   })
 
   $('.rave').on('click', function(){
@@ -111,7 +108,7 @@ $(function(){
     $(this).siblings().removeClass('hidden');
     $(this).addClass('hidden')
 
-    animatorObject.switchRaveState();
-    animatorObject.checkRaveState();
+    switchRaveState();
+    checkRaveState();
   })
 });
