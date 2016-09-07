7ffef7fdadfadbe6234287da159e2d68a71294e2
Jonathan Norstrom <jon.norstrom@gmail.com>
2016-09-07 00:16:21 -0500

    lots of refactor

--- public/application.js
+++ public/application.js
@@ -1,9 +1,9 @@
 var captionLength = 0;
 var captionOptions = ["Oh, hello there.", "So this is my page.", "Hope you get what you need :)"]
 var captionIndex = 0;
-
 var rgbOptions = ["rgb(61, 183, 88)", "rgb(230, 79, 59)", "rgb(222, 77, 99)", "rgb(211, 188, 180)", "rgb(255, 255, 255)", "rgb(255, 220, 90)", "rgb(77, 222, 171)"]
 var colorIndex = 0;
+var currentlyRaving = false;
 
 function runCaptions() {
   cursorAnimation();
@@ -52,20 +52,55 @@ function toggleMethods() {
   $('.methods').toggle('slide');
 }
 
-function raveTime() {
+function switchDomText() {
+  var $domText = $('.rave').html();
+  if ($domText == "Boring Time") {
+    $('.rave').html('Party Time')
+  } else {
+    $('.rave').html('Boring Time')
+  }
+}
+
+function switchRaveState() {
+  if (currentlyRaving) {
+    currentlyRaving = false
+  } else {
+    currentlyRaving = true
+  }
+}
+
+function checkRaveState() {
+  if (currentlyRaving) {
+    animateBackgroundColor(60);
+    play();
+    setTimeout('checkRaveState()', 100);
+  } else {
+    stopAndReset();
+  }
+}
+
+function animateBackgroundColor(speed) {
   if (colorIndex == rgbOptions.length) {
     colorIndex = 0;
   }
-  $('html').animate({"background-color": rgbOptions[colorIndex]}, 60);
+  $('html').animate({"background-color": rgbOptions[colorIndex]}, speed);
   colorIndex++
-  setTimeout('raveTime()', 100);
 }
 
-function play(){
-  document.getElementById("audio").play();
+function play() {
+  var audio = $("#audio")
+  // if (audio.paused) {
+    $("#audio").play();
+  // }
 }
 
-$(document).ready(function(){
+function stopAndReset() {
+  var audio = $("#audio")
+  audio.stop();
+  audio.currentTime = 0
+}
+
+$(function(){
   runCaptions();
 
   $('.message a').on('click', function(e) {
@@ -75,19 +110,13 @@ $(document).ready(function(){
 
   $('.color-change p.background').on('click', function(e) {
     e.preventDefault();
-    if (colorIndex == rgbOptions.length) {
-      colorIndex = 0;
-    }
-    $('html').animate({"background-color": rgbOptions[colorIndex]})
-    colorIndex++
+    animateBackgroundColor(350)
   })
 
   $('.rave').on('click', function(e){
-    if ($(this).html() == "Party Time") {
-      e.preventDefault();
-      raveTime(colorIndex);
-      $(this).html("Boring Time");
-      play();
-    }
+    e.preventDefault();
+    switchRaveState();
+    switchDomText();
+    checkRaveState();
   })
 });
